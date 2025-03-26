b2f4518a614b49cab91426273758236104c0e930
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-03-26 10:11:14 +0530

    sort function optimized

--- assets/js/sortFns.js
+++ assets/js/sortFns.js
@@ -1,48 +1,39 @@
 // function to sort books by title
 function sortBooks(type, fetchedBooks) {
+  // variables to store sorted books and sort type
+  let sortedBooks, elementType;
+
   if (type === "title") {
-    // sort books on basis of title
-    const sortedBooks = fetchedBooks.sort((a, b) => a.title.localeCompare(b.title));
+    // store sorted books on basis of title
+    sortedBooks = fetchedBooks.sort((a, b) => a.title.localeCompare(b.title));
 
-    // define default order
-    let defOrder = 1;
-
-    // change order of each book element on the basis of sorted array books
-    sortedBooks.forEach((book) => {
-      // find book by title
-      const findBooksElement = document.querySelectorAll(`[title="${book.title}"]`);
-
-      // get each book element if multiple found
-      findBooksElement.forEach((bookElement) => {
-        // change order of current book
-        bookElement.style.order = defOrder;
-
-        // increase order for next book
-        defOrder++;
-      });
-    });
+    // set the element type to title for searching in HTML elements
+    elementType = "title";
   } else {
-    // sort books on basis of published date
-    const sortedBooks = fetchedBooks.sort((a, b) => new Date(a.publishedDate) - new Date(b.publishedDate));
+    // store sorted books on basis of published date
+    sortedBooks = fetchedBooks.sort((a, b) => new Date(a.publishedDate) - new Date(b.publishedDate));
 
-    // define default order
-    let defOrder = 1;
-
-    // change order of each book element on the basis of sorted array books
-    sortedBooks.forEach((book) => {
-      // find book by published date
-      const findBooksElement = document.querySelectorAll(`[publishedDate="${book.publishedDate}"]`);
-
-      // get each book element if multiple found
-      findBooksElement.forEach((bookElement) => {
-        // change order of current book
-        bookElement.style.order = defOrder;
-
-        // increase order for next book
-        defOrder++;
-      });
-    });
+    // set the element type to published date for searching in HTML elements
+    elementType = "publishedDate";
   }
+
+  // define default order
+  let defOrder = 1;
+
+  // change order of each book element on the basis of sorted array books
+  sortedBooks.forEach((book) => {
+    // find books
+    const findBooksElement = document.querySelectorAll(`[${elementType}="${book[elementType]}"]`);
+
+    // get each book element if multiple found
+    findBooksElement.forEach((bookElement) => {
+      // change order of current book
+      bookElement.style.order = defOrder;
+
+      // increase order for next book
+      defOrder++;
+    });
+  });
 }
 
 // export functions
